f30d36b86ab8f4296d4581b8ea0032afd955afd7
Caio <caio8200@gmail.com>
2022-07-20 21:11:08 -0300

    att: final project base

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import {createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Projects from '../views/Projects.vue'
+import Contact from '../views/Contact.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path: '/about',
+      component: About
+    },
+    {
+      path: '/projects',
+      component: Projects
+    },
+    {
+      path: '/contact',
+      component: Contact
+    }
+  ]
+})
+
+export default router;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="home-page">
     <h1>Home</h1>
-    <p>Esta é a home page</p>
+    <p>Esta é a home page do boneco</p>
   </main>
 </template>
